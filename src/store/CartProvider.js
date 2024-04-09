e04067838d4c5abb8517b93aa7fa6833d00cd655
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-04-09 14:13:02 +0545

    Added add/remove function through sign in cart and little animation in cart using useEffect

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,14 +11,53 @@ const cartReducer= (state, action)=> {
     
     if(action.type ==='ADD'){
 
-        const updatedItems = state.items.concat(action.item); //concat is a build in method that don't edit
-        //old state/arry but gives brand new array
-        const updatedTotalAmount= state.totalAmount + action.item.price * action.item.amount  ;
+        const updatedTotalAmount= state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex= state.items.findIndex(
+            (item)=>item.id === action.item.id);
+        //findIndex is js inbuild function witch will find index for each item in arry
+        //findindex will return index value of item in arry
+
+        const existingCartItem= state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem){
+            const updatedItem= {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems= [...state.items];
+            updatedItems[existingCartItemIndex]= updatedItem;
+        } else{
+             updatedItems = state.items.concat(action.item); //concat is a build in method that don't edit
+            //old state/arry but gives brand new array
+        }
         return{
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
     };
+    if(action.type === 'REMOVE'){
+        const existingCartItemIndex= state.items.findIndex(
+            (item)=>item.id === action.id);
+        
+        const existingItem= state.items[existingCartItemIndex];
+        const updatedTotalAmount= state.totalAmount - existingItem.price;
+        let updatedItems;
+        if(existingItem.amount === 1){
+            updatedItems= state.items.filter((item)=>item.id !== action.id);
+            //filter is a build in function that stores value in new arry if the function is true
+            //if it returns false then the item is removed from array if the function is false
+        }else{
+            const updatedItem= {...existingItem, amount:existingItem.amount - 1};
+            updatedItems= [...state.items];
+            updatedItems[existingCartItemIndex]= updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
     return defaultCartState;
 };
 
